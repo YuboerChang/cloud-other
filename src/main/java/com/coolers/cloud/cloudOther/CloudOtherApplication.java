b4ff11cc9f4402f16bb325aee4361139c4565044
Yubo_Zhang <1146555898@qq.com>
2024-11-19 18:26:45 +0800

    1-fix-pom-parent-find-2-change-eureka-use

--- src/main/java/com/coolers/cloud/cloudOther/CloudOtherApplication.java
+++ src/main/java/com/coolers/cloud/cloudOther/CloudOtherApplication.java
@@ -3,10 +3,10 @@ package com.coolers.cloud.cloudOther;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class CloudOtherApplication {
 
     public static void main(String[] args) {
